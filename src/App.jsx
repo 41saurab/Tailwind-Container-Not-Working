dce59000c2f6d3b03741dc6726fa9d73c745da9a
saurab <41saurab@gmail.com>
2024-09-16 19:43:30 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="container bg-red-500">
+        <h1 className="text-3xl font-serif">Hello World!!</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
